644cea29a06d4964f55a02159a442c2457cb0922
NRPM <nirupampatil44@gmail.com>
2022-10-14 22:20:52 +0530

    added clicky crates unity files to git repo

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton;
+    private int score;
+    private float spawnRate = 1f;
+    
+
+    public bool isGameActive;
+    // Start is called before the first frame update
+    void Start()
+    {
+     
+
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targets.Count);
+            Instantiate(targets[index]);
+        }
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;    
+        scoreText.text = "Score: " + score;
+    }
+
+    public void GameOver()
+    {
+        restartButton.gameObject.SetActive(true);
+        gameOverText.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        titleScreen.SetActive(false);
+        isGameActive = true;
+        score = 0;
+        scoreText.text = "Score: " + score;
+        UpdateScore(0);
+        StartCoroutine(SpawnTarget());
+
+    }
+}
